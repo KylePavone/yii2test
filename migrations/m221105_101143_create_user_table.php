cf661f483e235f228a51624ad3e35bde37bd0444
KylePavone <scaryfase1237373@gmail.com>
2022-11-09 18:41:41 +0300

    fixed

--- migrations/m221105_101143_create_user_table.php
+++ migrations/m221105_101143_create_user_table.php
@@ -8,10 +8,6 @@ use yii\db\Migration;
  */
 class m221105_101143_create_user_table extends Migration
 {
-    public function randomPassword() {
-        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        return substr(str_shuffle($permitted_chars), 0, 10);
-    }
     /**
      * {@inheritdoc}
      */
@@ -21,7 +17,7 @@ class m221105_101143_create_user_table extends Migration
             'id' => $this->primaryKey(),
             'name' => $this->string(64)->notNull(),
             'email' => $this->string(100)->notNull(),
-            'password' => $this->string(100)->notNull(),//->defaultValue($this->randomPassword()),
+            'password' => $this->string(100)->notNull(),
             'date_reg' => $this->timestamp(),
         ]);
       /*  */
